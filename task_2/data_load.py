ac94d2a604d964fff1216fae6fad6a7d37cdda4b
Sergei Dudoladov <avvallack@gmail.com>
2021-05-18 11:52:59 +0300

    established pipeline

---
+++ task_2/data_load.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def bring_to_float(x):
+    if type(x) == float:
+        return x
+    return float(x.replace(',', '.'))
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('AirQualityUCI.csv', sep=';',
+                     infer_datetime_format=True, usecols=['CO(GT)', 'PT08.S1(CO)', 'NMHC(GT)',
+                                                          'C6H6(GT)', 'PT08.S2(NMHC)', 'NOx(GT)', 'PT08.S3(NOx)',
+                                                          'NO2(GT)', 'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'RH', 'AH'])
+
+    for col in ['CO(GT)', 'C6H6(GT)', 'T', 'RH', 'AH']:
+        df[col] = df[col].apply(bring_to_float)
+
+    df.dropna(inplace=True)
+
+    df.to_csv('prepared_data.csv', index=False)
+
